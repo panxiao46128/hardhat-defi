c056586ef55786e894a09bd1d31f23a2be59b84b
kaw-yi <46128884@qq.com>
2023-04-03 12:48:07 +0800

    second change

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -5,16 +5,16 @@ const AMOUNT = ethers.utils.parseEther("0.1")
 
 async function getWeth() {
     const { deployer } = await getNamedAccounts()
-    // const iWeth = await ethers.getContractAt(
-    //     "IWeth",
-    //     networkConfig[network.config.chainId].wethToken,
-    //     deployer
-    // )
-    // const txResponse = await iWeth.deposit({
-    //     value: AMOUNT,
-    // })
-    // await txResponse.wait(1)
-    // const wethBalance = await iWeth.balanceOf(deployer)
-    // console.log(`Got ${wethBalance.toString()} WETH`)
+    const iWeth = await ethers.getContractAt(
+        "IWeth",
+        networkConfig[network.config.chainId].wethToken,
+        deployer
+    )
+    const txResponse = await iWeth.deposit({
+        value: AMOUNT,
+    })
+    await txResponse.wait(1)
+    const wethBalance = await iWeth.balanceOf(deployer)
+    console.log(`Got ${wethBalance.toString()} WETH`)
 }
 module.exports = { getWeth, AMOUNT }
